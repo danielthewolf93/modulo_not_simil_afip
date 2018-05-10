d51b3e1d2858a5bb9af410c8e3ff5e8c67d07eb3
Daniel White <danieltesorero2009@gmail.com>
2018-05-10 13:15:42 -0300

    cambios_nuevos_intimacion

--- resources/views/intim.blade.php
+++ resources/views/intim.blade.php
@@ -1,15 +1,61 @@
+@extends('layouts.app')
 
 
- 								<div class="form-group" >
+@section('content')				
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Generador</div>
+
+				 <form method="POST" action="{{ route('messages.store')}}" enctype="multipart/form-data">
+                    {{ csrf_field() }}
+
+								<div class="panel-body">
+ 								   <div class="form-group">
+ 									    <label class="form-group">Modelo de Intimacion</label>
 	                                    <select class="form-control" name="modeloform">
 	                                    <option value="0">Seleccionar Modelo</option> 
 	                                    <option value="1">Modelo 1</option>
 	                                    <option value="2">Modelo 2</option>    
 	                                    <option value="3">Modelo 3</option>
 	                                    </select>
-                                </div>
+                                	 </div>
+									
+									<div class="form-group">
+										<input type="text" name="cuit" placeholder="cuit_contribuyente">
+									</div>
+
+									<div class="form-group">
+										<select>
+											<option value="0">Seleccionar</option>
+											<option value="1">Matricula1</option>
+											<option value="2">Matricula2</option>
+											<option value="3">Matricula3</option>
+										</select>
+									</div>
+
+
+                                	 <div class="form-group">
+                                	 	<input type="date" name="fecha_hoy" >
+									</div>
+									
+
+
+                           		 
 
                                 <div class="form-group">
                                 <a href="{{ route('model1') }}" class="btn btn-primary ">Generar Modelo</a>
                                 </div>
 
+                              </div>
+
+
+                      </div>
+
+                 </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
