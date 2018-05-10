ef63ea08e310aeee8587e43ace904f3c5cc36c49
Daniel White <danieltesorero2009@gmail.com>
2018-05-10 11:05:09 -0300

    estetic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Inicio</div>
+                <div class="panel-heading">Novedades {{ date(' j-m-Y') }}    </div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -18,7 +18,7 @@
 
                     
 
-                    Bienvenido contribuyente
+                    <p>*Mantenimiento de 15 a 17hs.</p> 
                 </div>
             </div>
         </div>
