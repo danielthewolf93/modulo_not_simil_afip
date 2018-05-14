174bd76660a6efec3315dd9561c63a100fce10cc
Daniel White <danieltesorero2009@gmail.com>
2018-05-14 11:40:22 -0300

    mejorar_tabla_vista_dejar_rom

--- resources/views/vistacontr.blade.php
+++ resources/views/vistacontr.blade.php
@@ -4,7 +4,11 @@
 
 @section('content')
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
 
 
 <div class="container">
@@ -17,19 +21,129 @@
 
 <h1><i &nbsp class="fa fa-address-card" ></i>&nbspJuan Perez</h1>
 <h3>&nbsp&nbsp&nbspCuit 20-37462532-3</h3>
+<br>
+<br>
+
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
 
 
 
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
 
+<br>
+	<h3>Notificaciones</h3>
+<br>
+	
 
+	<table class="table">
+    <thead>
+      <tr>
+      	<th>Fecha</th>
+        <th>Tema</th>
+        <th>Despacho</th>
+        <th>Contribuyente</th>
+        <th>Adjunto</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Default</td>
+        <td>Defaultson</td>
+        <td>def@somemail.com</td>
+        <td>Daniel Blanco</td>
+		<td><i &nbsp class="glyphicon glyphicon-paperclip	
+		" ></i></td>
+      </tr>      
+      <tr class="table-primary">
+        <td>Primary</td>
+        <td>Joe</td>
+        <td>joe@example.com</td>
+        <td>Daniel Blanco</td>
+        <td></td>
+      </tr>
+      <tr class="table-success">
+        <td>Success</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+        <td>Daniel Blanco</td>
+        <td></td>
+      </tr>
+      <tr class="table-danger">
+        <td>Danger</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+        <td>Daniel Blanco</td>
+        <td><i &nbsp class="glyphicon glyphicon-paperclip	
+		" ></i></td>
+      </tr>
+      <tr class="table-info">
+        <td>Info</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+        <td>Daniel Blanco</td>
+        <td></td>
+      </tr>
+      <tr class="table-warning">
+        <td>Warning</td>
+        <td>Refs</td>
+        <td>bo@example.com</td>
+        <td>Daniel Blanco</td>
+        <td></td>
+      </tr>
+      <tr class="table-active">
+        <td>Active</td>
+        <td>Activeson</td>
+        <td>act@example.com</td>
+        <td>Daniel Blanco</td>
+        <td></td>
+      </tr>
+      <tr class="table-secondary">
+        <td>Secondary</td>
+        <td>Secondson</td>
+        <td>sec@example.com</td>
+        <td>Daniel Blanco</td>
+        <td></td>
+      </tr>
+      <tr class="table-light">
+        <td>Light</td>
+        <td>Angie</td>
+        <td>angie@example.com</td>
+        <td>Daniel Blanco</td>
+        <td></td>
+      </tr>
+      <tr class="table-dark text-dark">
+        <td>Dark</td>
+        <td>Bo</td>
+        <td>bo@example.com</td>
+        <td>Daniel Blanco</td>
+        <td><i &nbsp class="glyphicon glyphicon-paperclip	
+		" ></i></td>
+      </tr>
+    </tbody>
+  </table>
+
+			</div>
+
+		</div>
+
+	</div>
 
 </div>
 
-</div>
 
-</div>
+	
 
-</div>
+
 
 
 
