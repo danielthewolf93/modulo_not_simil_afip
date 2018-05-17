2a95a40905881c875ed1a7164ebe35ade7f303d0
Daniel White <danieltesorero2009@gmail.com>
2018-05-17 11:43:46 -0300

    cambios_actualizac_mejorar

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,6 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
  <meta charset="UTF-8">
  <link href="/css/app.css" rel="stylesheet">
  <style>
@@ -14,41 +11,3 @@
     <div class="container">
  <div class=" row">
       <div class="col-md-8 col-md-offset-2">
-          <div class="panel panel-default">
-              <div class="panel-heading">Formulario</div>
-              <div class="panel-body">
- @include('messages')
-                  <form class="form-horizontal" role="form" method="POST" action="{{ url('formus') }}">
-                      {{ csrf_field() }}
- 
-                         <div class="form-group">
-                             <label for="student" class="col-md-4 control-label">Estudiante</label>
- 
-                             <div class="col-md-6">
-                                 <input id="student" type="text" class="form-control" name="student" value="{{ old('student') }}">
-                             </div>
-                         </div>
- 
-                         <div class="form-group">
-                             <label for="score" class="col-md-4 control-label">Puntuación</label>
- 
-                             <div class="col-md-6">
-                                 <input id="score" class="form-control" name="score" value="{{ old('score') }}" >
-                             </div>
-                         </div>
- 
-                         <div class="form-group">
-                             <div class="col-md-6 col-md-offset-4">
-                                 <button type="submit" class="btn btn-primary">
-                                     Enviar
-                                 </button>
-                             </div>
-                         </div>
-                     </form>
-                 </div>
-             </div>
-         </div>
-     </div>
- </div>
-   </body>
-</html>
